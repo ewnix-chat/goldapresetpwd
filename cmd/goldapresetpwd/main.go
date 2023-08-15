dac0a9a18e614f10e0043380a2a3fb2703947560
Kevin Thompson <phlux@ewnix.net>
2023-08-15 12:03:59 -0500

    Update our password hashing. LDAP doesn't like bcrypt for some reason.

--- cmd/goldapresetpwd/main.go
+++ cmd/goldapresetpwd/main.go
@@ -2,60 +2,77 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/go-ldap/ldap/v3"
+	"github.com/rs/cors"
+	"github.com/simia-tech/crypt"
 	"log"
 	"net/http"
 	"os"
-	"github.com/go-ldap/ldap/v3"
-	"github.com/rs/cors"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type ResetPasswordRequest struct {
-	Username       string `json:"username"`
+type PasswordResetRequest struct {
+	Username        string `json:"username"`
 	CurrentPassword string `json:"currentPassword"`
 	NewPassword     string `json:"newPassword"`
 }
 
-func hashPassword(password string) string {
-	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(hashedPassword)
-}
-
-func resetPasswordHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var req ResetPasswordRequest
-	json.NewDecoder(r.Body).Decode(&req)
-
-	ldapServer := os.Getenv("LDAP_SERVER")
-	ldapPort := os.Getenv("LDAP_PORT")
-	ldapUserDN := os.Getenv("LDAP_USER_DN")
-
-	l, _ := ldap.Dial("tcp", ldapServer+":"+ldapPort)
-	defer l.Close()
-
-	bindDN := "cn=" + req.Username + "," + ldapUserDN
-	l.Bind(bindDN, req.CurrentPassword)
-
-	modifyRequest := ldap.NewModifyRequest(bindDN, nil)
-	modifyRequest.Replace("userPassword", []string{hashPassword(req.NewPassword)})
-	l.Modify(modifyRequest)
-
-	w.WriteHeader(http.StatusOK)
-}
-
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/reset", resetPasswordHandler)
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		var req PasswordResetRequest
+		err := decoder.Decode(&req)
+		if err != nil {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		ldapServer := os.Getenv("LDAP_SERVER")
+		ldapPort := os.Getenv("LDAP_PORT")
+		ldapUserDN := os.Getenv("LDAP_USER_DN")
+
+		l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%s", ldapServer, ldapPort))
+		if err != nil {
+			http.Error(w, "Failed to connect to LDAP server", http.StatusInternalServerError)
+			return
+		}
+		defer l.Close()
+
+		err = l.Bind(fmt.Sprintf("cn=%s,%s", req.Username, ldapUserDN), req.CurrentPassword)
+		if err != nil {
+			http.Error(w, "Failed to bind to LDAP server", http.StatusUnauthorized)
+			return
+		}
+
+		salt := crypt.GenerateSalt(crypt.SHA256)
+		cryptedPassword, err := crypt.Crypt([]byte(req.NewPassword), salt)
+		if err != nil {
+			http.Error(w, "Error crypting password", http.StatusInternalServerError)
+			return
+		}
+
+		modifyRequest := ldap.NewModifyRequest(fmt.Sprintf("cn=%s,%s", req.Username, ldapUserDN), nil)
+		modifyRequest.Replace("userPassword", []string{"{CRYPT}" + string(cryptedPassword)})
+		err = l.Modify(modifyRequest)
+		if err != nil {
+			http.Error(w, "Failed to update password", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Password updated successfully"))
+	})
 
 	handler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://www.ewnix.net"},
-		AllowedMethods:   []string{"GET", "POST"},
-		AllowedHeaders:   []string{"Authorization", "Content-Type"},
-		AllowCredentials: true,
+		AllowedOrigins: []string{"https://www.ewnix.net"},
+		AllowedMethods: []string{"GET", "POST"},
+		AllowedHeaders: []string{"Authorization", "Content-Type"},
 	}).Handler(mux)
 
 	log.Fatal(http.ListenAndServe(":8080", handler))
--- go.mod
+++ go.mod
@@ -7,5 +7,7 @@ require (
 	github.com/go-asn1-ber/asn1-ber v1.5.4 // indirect
 	github.com/go-ldap/ldap/v3 v3.4.5 // indirect
 	github.com/rs/cors v1.9.0 // indirect
+	github.com/simia-tech/crypt v0.5.1 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 )
